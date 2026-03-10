76cf7757f49555f8854d7b9f413cefd4a67e69f0
MoathRamzi <moathramzi18@gmail.com>
2026-03-11 00:49:29 +0300

    Add project files.

---
+++ Assaiment_successfully_withClass/Classes/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assaiment_successfully_withClass.Classes
+{
+    internal class Class1
+    {
+        public void printResult(string input) 
+        {
+            string str = input;
+
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+
+            foreach (char c in str)
+            {
+                if (dic.ContainsKey(c))
+                {
+                    dic[c]++;
+                }
+                else
+                {
+                    dic[c] = 1;
+                }
+            }
+
+            foreach (var item in dic)
+            {
+                Console.WriteLine($" {item.Key} :  {item.Value} ");
+            }
+        }
+    }
+
+}
---
+++ Assaiment_successfully_withClass/Program.cs
@@ -0,0 +1,7 @@
+﻿using Assaiment_successfully_withClass.Classes;
+
+string str = Console.ReadLine();
+
+Class1 user =  new Class1();
+
+user.printResult(str);
